fd97ccbab6c3a1fb7b5a9bbbda57290ad85aa250
Javi <javierplazasisques@gmail.com>
2020-05-20 17:16:51 +0200

    20/05/2020

--- Maceta-Arduino.ino
+++ Maceta-Arduino.ino
@@ -32,7 +32,7 @@ String IP = "161.35.69.225";
 String URL = "http://" + IP + ":" + PORT + "/gardenia/api/flowerpots";
 String URL_UPDATE = URL + "/update";
 String MAC = WiFi.softAPmacAddress();
-String VERSION = "0.2";
+String VERSION = "0.3";
 
 //Constants screen
 #define SCREEN_WIDTH 128
